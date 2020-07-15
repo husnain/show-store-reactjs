98ea2140d512d36c71608e232c9f4e14a3569b22
Husnain Ahmed <husnain.ahmed@skillorbit.com>
2020-07-16 01:21:03 +0500

    updating home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
     <Main title="Welcome to Shoe Store" description="Shoe Store Shopping Cart" >
       <div className="text-center mt-5">
         <h1>Shoe Store</h1>
+        <p>Welcome to Husnain Ahmed's Shoe Store</p>
       </div>
       <div className="text-center mb-20">
         <Button variant="contained" color="secondary">
